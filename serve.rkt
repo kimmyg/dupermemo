9d2ab73dc8a3fa206e3049db2b49428fa86141d5
Kimball Germane <krgermane@yahoo.com>
2012-10-23 14:58:04 -0600

    What?

--- serve.rkt
+++ serve.rkt
@@ -2,8 +2,10 @@
 (require web-server/servlet
          web-server/servlet-env)
 
+(define facts empty)
+
 (define (start request)
-  (page/add-facts request))
+  (page/main request))
 
 (define (page/main request)
   (response/xexpr
@@ -15,20 +17,56 @@
       (p (a ([href "#"]) "review facts"))
       (p (a ([href "#"]) "add facts"))))))
 
+(define (can-parse-queued-fact? bindings)
+  #t)
+;  (and (exists-binding? 'prompt bindings)
+;       (exists-binding? 'response bindings)
+;       (and (exists-binding? 'tags bindings)
+;            (for/and ([tag (string-split (extract-binding/single 'tags bindings))])
+;              (rexexp-match? #rx"^\\w$" tag)))
+;       (and (exists-binding? 'priority bindings)
+            
+(define (parse-queued-fact bindings)
+  (apply queued-fact (for/list ([symbol (list 'prompt 'response)])
+                       (extract-binding/single symbol bindings))))
+      
+
+(define (fact->id fact)
+  (
+
+(if (can-parse-queued-fact? bindings)
+    (begin
+    (let ([fact (parse-queued-fact bindings)])
+      (open-output-file (string-append "queue/" (fact->id fact)
+
+(define (page/add-fact request)
+  (send/suspend/dispatch
+   (λ (embed/url)
+     (response/xexpr
+      `(html
+        (head
+         (title "Add Fact"))
+        (body
+         (form ([action ,k-url] [method "post"])
+               (p "prompt" (input ([type "text"])))
+               (p "response" (input ([type "text"])))
+               (p "priority" (input ([type "text"])))
+               (p "tags" (input ([type "text"])))
+               (p (input ([type "submit"] [value "add fact"])))))))))])
 (define (page/add-facts request)
   (let ([request2 (send/suspend
-                  (λ (k-url)
-                    (response/xexpr
-                     `(html
-                       (head
-                        (title "add fact"))
-                       (body
-                        (form ([action ,k-url] [method "post"])
-                              (p "prompt" (input ([type "text"])))
-                              (p "response" (input ([type "text"])))
-                              (p "priority" (input ([type "text"])))
-                              (p "tags" (input ([type "text"])))
-                              (p (input ([type "submit"] [value "add fact"])))))))))])
+                   (λ (k-url)
+                     (response/xexpr
+                      `(html
+                        (head
+                         (title "add fact"))
+                        (body
+                         (form ([action ,k-url] [method "post"])
+                               (p "prompt" (input ([type "text"])))
+                               (p "response" (input ([type "text"])))
+                               (p "priority" (input ([type "text"])))
+                               (p "tags" (input ([type "text"])))
+                               (p (input ([type "submit"] [value "add fact"])))))))))])
     (begin
       (request-bindings request2)
       
